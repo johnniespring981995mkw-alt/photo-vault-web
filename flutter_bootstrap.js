f2698a98513dd21bda36adbafd905dc71be030e3
johnniespring981995mkw-alt <johnniespring981995mkw-alt@users.noreply.github.com>
2026-07-16 08:49:56 +0000

    deploy: e0af3706f9998aeb0f9a8a9e2849b492f45ab5ce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2339639349" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1834881816" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
