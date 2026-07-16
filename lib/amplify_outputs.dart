5dc0ac5ca03e8384896135bc33a8ec247fd50be3
johnniespring981995mkw-alt <johnniespring981995mkw@gmail.com>
2026-07-16 14:04:54 +0700

    Upload lib/amplify_outputs.dart via Antigravity Auto-Deployer

--- lib/amplify_outputs.dart
+++ lib/amplify_outputs.dart
@@ -1,24 +1,58 @@
 const amplifyConfig = '''{
-  "version": "1",
   "auth": {
-    "aws_region": "us-east-1",
-    "user_pool_id": "us-east-1_placeholder",
-    "user_pool_client_id": "placeholderclientid",
-    "identity_pool_id": "us-east-1:placeholder-identity-pool-id",
-    "standard_required_attributes": ["email"],
-    "username_attributes": ["email"],
-    "user_verification_types": ["email"],
-    "unauthenticated_identities_enabled": true,
+    "user_pool_id": "ap-southeast-1_QWojomuYD",
+    "aws_region": "ap-southeast-1",
+    "user_pool_client_id": "7grmh4ikdsr6mui7qlt6qen7b3",
+    "identity_pool_id": "ap-southeast-1:8ff84085-b724-4f5d-b43f-64eb76053c05",
+    "mfa_methods": [],
+    "standard_required_attributes": [
+      "email"
+    ],
+    "username_attributes": [
+      "email"
+    ],
+    "user_verification_types": [
+      "email"
+    ],
+    "groups": [],
+    "mfa_configuration": "NONE",
     "password_policy": {
       "min_length": 8,
       "require_lowercase": true,
-      "require_uppercase": true,
       "require_numbers": true,
-      "require_symbols": true
-    }
+      "require_symbols": true,
+      "require_uppercase": true
+    },
+    "unauthenticated_identities_enabled": true
   },
   "storage": {
-    "aws_region": "us-east-1",
-    "bucket_name": "placeholder-bucket-name"
-  }
+    "aws_region": "ap-southeast-1",
+    "bucket_name": "amplify-d108r8994u20x9-ma-photobackupdrivebucketb1-tiaqtds1joi6",
+    "buckets": [
+      {
+        "name": "photoBackupDrive",
+        "bucket_name": "amplify-d108r8994u20x9-ma-photobackupdrivebucketb1-tiaqtds1joi6",
+        "aws_region": "ap-southeast-1",
+        "paths": {
+          "full/\${cognito-identity.amazonaws.com:sub}/*": {
+            "entityidentity": [
+              "get",
+              "list",
+              "write",
+              "delete"
+            ]
+          },
+          "thumb/\${cognito-identity.amazonaws.com:sub}/*": {
+            "entityidentity": [
+              "get",
+              "list",
+              "write",
+              "delete"
+            ]
+          }
+        }
+      }
+    ]
+  },
+  "version": "1.3"
 }''';
